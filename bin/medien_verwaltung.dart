6586fc97468d8806c16dd6b9896f8e1886befcb4
lanm <markus.langenegger@kilchenmann.ch>
2023-10-27 15:27:37 +0200

    initial commit

---
+++ bin/medien_verwaltung.dart
@@ -0,0 +1,5 @@
+import 'package:medien_verwaltung/medien_verwaltung.dart' as medien_verwaltung;
+
+void main(List<String> arguments) {
+  print('Hello world: ${medien_verwaltung.calculate()}!');
+}
---
+++ test/medien_verwaltung_test.dart
@@ -0,0 +1,8 @@
+import 'package:medien_verwaltung/medien_verwaltung.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
